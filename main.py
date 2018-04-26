fa44ca03da9791572ce0b46a5703eeb6a324b0a9
Giles Johnson <bastisawesomeltd@gmail.com>
2018-04-26 19:59:13 -0400

    Remove temporary code

--- main.py
+++ main.py
@@ -2,24 +2,9 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QCoreApplication
 
-import rpiMorse
-from rpiMorse import PINS
 from mainwindow import MainWindow
 
 def main():
-    # rpiMorse.setup()
-    #
-    # userInput = ""
-    # while True:
-    #     userInput = input("Line (q to quit): ")
-    #
-    #     if userInput.lower() == "q" or userInput.lower() == "quit": break
-    #
-    #     code = rpiMorse.parseLine(userInput)
-    #     rpiMorse.outputCode(code, PINS.GPIO18)
-    #
-    # rpiMorse.cleanup()
-    
     # Configure the application
     QCoreApplication.setApplicationName("RPiMorse")
     
